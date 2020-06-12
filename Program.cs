0713c126be5e96e74801d06c78040d7e2f32cc0b
Jhampton64 <63685850+Jhampton64@users.noreply.github.com>
2020-06-12 12:08:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C.NET_FRAMEWORK_COURSE_101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            bool candrive = (age >= 15 && dui == "false" && ticket < 3);
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(candrive);
+            Console.ReadLine();
+        }
+    }
+}
